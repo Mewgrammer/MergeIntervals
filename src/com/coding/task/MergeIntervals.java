2b0df6f8280c1c0bb8ce946a7b3453ec889a839f
Michael.Mew <michael4mew@gmail.com>
2019-11-20 19:32:59 +0100

    move to single file

--- src/com/coding/task/MergeIntervals.java
+++ src/com/coding/task/MergeIntervals.java
@@ -2,12 +2,41 @@
     Program to merge intervals
  */
 
-package com.coding.task;
 
 import java.util.*;
 
 
-public class Main {
+/**
+ * Interval Class.
+ */
+class Interval {
+    private int start;
+    private int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+}
+
+
+public class MergeIntervals {
     /**
      * Main program.
      * @param args Commandline arguments
